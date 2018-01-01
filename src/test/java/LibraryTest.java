1bbd79da76f6b43dd660a88cf8456f0bfd37a222
eyal.kopelevich <eyal.kopelevich@EYALKOPEL-LT>
2018-01-01 15:06:14 +0200

    j

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,9 +1,12 @@
 
 import com.experitest.client.*;
+import com.experitest.client.log.ILogger;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
 public class LibraryTest {
 
